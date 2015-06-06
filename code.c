1d4edfef6b0f1a3fdd4944e709b51f0cf3baf917
castle-yellow <castle_yellow@163.com>
2015-06-06 14:51:54 +0800

    Delete code.c

--- code.c
+++
@@ -1,6 +0,0 @@
-obj-m := syscall.o
-KERNELDIR := /lib/modules/$(shell uname –r)/build PWD := $(shell pwd)
-modules:
-$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
-modules_install:
-$(MAKE) -C $(KERNELDIR) M=$(PWD) modules_install
